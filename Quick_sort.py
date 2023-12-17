744232a78f861d79de82a88d771497c784b34b97
Yash Rajput <02yashrajput@gmail.com>
2023-12-17 16:52:59 +0530

    commit

--- Quick_sort.py
+++ Quick_sort.py
@@ -1,21 +1,20 @@
 def partition(arr,start,end):
-    i = start-1
     pivot = arr[end]
+    i = start-1
     for j in range(start,end):
-        if(arr[j]<=pivot):
-            i+1
-            arr[i],arr[j]=arr[j],arr[i]
-        arr[i+1],arr[end]=arr[end],arr[i+1]
+        if arr[j]<=pivot:
+            i+=1
+            arr[j],arr[i]=arr[i],arr[j]
+    arr[i+1],arr[end] = arr[end],arr[i+1]
     return i+1
-            
+
 
 
 def quick_sort(arr,start,end):
-    if (start<end):
+    if start<end:
         p = partition(arr,start,end)
         quick_sort(arr,start,p-1)
         quick_sort(arr,p+1,end)
-        
 
 arr = [4,1,7,4,21,44,32,11]
 
